85d4fd7b347e8d90249051fea64b5195ad7ce3e6
em-ak <emilien.ak@gmail.com>
2014-02-03 11:40:07 +0100

    Create rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsGeoloc::Application.config.secret_key_base = '37d7f0ad6f34bd365df7fb3a62fe45c4f94700b8bd74da21736af7e008124f0f670a62ecdc42140e3bfa24dc197b489c0889364da2127ed4d7b54de3560f3b92'
